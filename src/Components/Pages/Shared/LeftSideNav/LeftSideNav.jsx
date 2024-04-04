02635a273aa1f14dd73e7d5562d72bbf6026c066
KMTonmoy <tonmoyahamd2009@gmail.com>
2024-04-04 10:19:56 +0600

    Left Side Compleat

--- src/Components/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Components/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -9,14 +9,38 @@ const LeftSideNav = () => {
             .then(data => setcatagories(data))
     }, [])
 
+    const [newses, setcatagoriessetNewes] = useState([])
+    useEffect(() => {
+        fetch('/public/news.json')
+            .then(res => res.json())
+            .then(data => setcatagoriessetNewes(data))
+    }, [])
+ 
+
     return (
         <div>
-            <h1 className='font-[600]'>All Category</h1>
-            <ul>
-                {
-                    catagories.map(catagorie => <li key={catagorie.id} className='text-gray-500 ml-5 mt-2'><Link to={`category/${catagorie.id}`}>{catagorie.name}</Link></li>)
-                }
-            </ul>
+            <div>
+                <h1 className='font-[600]'>All Category</h1>
+                <ul>
+                    {
+                        catagories.map(catagorie => <li key={catagorie.id} className='text-gray-500 ml-5 mt-2'><Link to={`category/${catagorie.id}`}>{catagorie.name}</Link></li>)
+                    }
+                </ul>
+            </div>
+            <div className='mt-10'>
+                {newses.slice(0, 3).map((news, index) => (
+                    <div key={index} className="card w-[267px] bg-base-100 shadow-xl mt-5">
+                        <figure><img src={news.image_url} alt="Shoes" /></figure>
+                        <div className="card-body p-5">
+                            <h2 className="text-2xl">{news.title}</h2>
+         
+                        </div>
+                    </div>
+                ))}
+
+            </div>
+
+
         </div>
     );
 };
